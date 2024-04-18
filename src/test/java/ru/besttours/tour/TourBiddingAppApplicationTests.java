1ce4ab92ef37b747a6335a5d83f8242d4cd45e0b
Spfeed <subbart2266@gmail.com>
2024-04-18 14:49:12 +0400

    Initial commit

---
+++ src/test/java/ru/besttours/tour/TourBiddingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.besttours.tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourBiddingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
